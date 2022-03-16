37411c6c3b6783805ae07a4703fcb0673b242a59
wandoy37 <riswandi035@gmail.com>
2022-03-16 08:18:19 +0800

    Form Login & Tamplating Dashboard

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+
+	function __construct()
+	{
+		parent::__construct();
+
+		date_default_timezone_set('Asia/Jakarta');
+
+		$this->load->model('m_data');
+
+		// cek session yang login, 
+		// jika session status tidak sama dengan session telah_login, berarti pengguna belum login
+		// maka halaman akan di alihkan kembali ke halaman login.
+		if ($this->session->userdata('status') != "telah_login") {
+			redirect(base_url() . 'login?alert=belum_login');
+		}
+	}
+
+
+	public function index()
+	{
+		$this->load->view('dashboard/v_index');
+	}
+
+	public function keluar()
+	{
+		$this->session->sess_destroy();
+		redirect('login?alert=logout');
+	}
+}
---
+++ application/views/v_login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Login | Swajasa</title>
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="<?php base_url(); ?>assets/img/icon.ico" type="image/x-icon" />
+
+    <!-- Fonts and icons -->
+    <script src="<?php base_url(); ?>assets/js/plugin/webfont/webfont.min.js"></script>
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Lato:300,400,700,900"]
+            },
+            custom: {
+                "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"],
+                urls: ['<?php base_url(); ?>assets/css/fonts.min.css']
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php base_url(); ?>assets/css/atlantis.css">
+</head>
+
+<body class="login">
+    <div class="wrapper wrapper-login">
+        <div class="container container-login animated fadeIn">
+
+            <h3 class="text-center">Sign In To Admin</h3>
+
+            <?php
+            if (isset($_GET['alert'])) {
+                if ($_GET['alert'] == "gagal") {
+                    echo "<div class='alert alert-danger font-weight-bold rounded'>Maaf! Username & Password Salah.</div>";
+                } else if ($_GET['alert'] == "belum_login") {
+                    echo "<div class='alert alert-danger font-weight-bold rounded'>Anda Harus Login Terlebih Dulu!</div>";
+                } else if ($_GET['alert'] == "logout") {
+                    echo "<div class='alert alert-success font-weight-bold rounded'>Anda Telah Logout!</div>";
+                }
+            }
+            ?>
+
+            <form action="<?php echo base_url() . 'login/aksi' ?>" method="post">
+                <div class="login-form">
+                    <div class="form-group form-floating-label">
+                        <input id="username" name="username" type="text" class="form-control input-border-bottom" required>
+                        <label for="username" class="placeholder">Username</label>
+                        <?php echo form_error('username'); ?>
+                    </div>
+
+                    <div class="form-group form-floating-label">
+                        <input id="password" name="password" type="password" class="form-control input-border-bottom" required>
+                        <label for="password" class="placeholder">Password</label>
+                        <div class="show-password">
+                            <i class="icon-eye"></i>
+                        </div>
+                        <?php echo form_error('password'); ?>
+                    </div>
+
+                    <div class="row form-sub m-0">
+                        <a href="#" class="link float-right">Forget Password ?</a>
+                    </div>
+                    <div class="form-action mb-3">
+                        <button type="submit" class="btn btn-primary btn-rounded btn-login">Sign In</button>
+                    </div>
+                    <div class="login-account">
+                        <span class="msg">Don't have an account yet ?</span>
+                        <a href="#" id="show-signup" class="link">Sign Up</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="<?php base_url(); ?>assets/js/core/jquery.3.2.1.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/core/popper.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/core/bootstrap.min.js"></script>
+    <script src="<?php base_url(); ?>assets/js/atlantis.min.js"></script>
+    <script>
+        window.setTimeout(function() {
+            $(".alert").fadeTo(300, 0).slideUp(300, function() {
+                $(this).remove();
+            });
+        }, 3000);
+    </script>
+</body>
+
+</html>
